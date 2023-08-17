9caabc144c854db623a8d52ae00c23d9f99d52fe
isanasan <yfcgpsebp@gmail.com>
2023-08-17 14:13:55 +0900

    fix: eks moduleを19系に上げる
    - ingress用のsecurity groupを生成してくれるようになったため削除
    - recommend security groupを利用
    - cluster endpointをpublicにすることでlocalからkubectlを使えるように

--- eks.tf
+++ eks.tf
@@ -1,7 +1,8 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
+  cluster_endpoint_public_access = true
 
   cluster_version = "1.27"
   cluster_name    = "${local.name_prefix}-eks"
@@ -14,13 +15,4 @@ module "eks" {
       instance_types = ["t3.medium"]
     }
   }
-}
-
-resource "aws_security_group_rule" "allow_ingress_port" {
-  security_group_id        = module.eks.node_security_group_id
-  type                     = "ingress"
-  from_port                = 9443
-  to_port                  = 9443
-  protocol                 = "tcp"
-  source_security_group_id = module.eks.cluster_security_group_id
 }
